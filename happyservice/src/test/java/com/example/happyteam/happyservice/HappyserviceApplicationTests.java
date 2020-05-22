e4be2f69d4fc5c7f6fbee47140a77ba4404b9484
saracastellino <sara.castellino0@gmail.com>
2020-05-22 14:23:17 +0100

    adds Spring setup

---
+++ happyservice/src/test/java/com/example/happyteam/happyservice/HappyserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.happyteam.happyservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HappyserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
