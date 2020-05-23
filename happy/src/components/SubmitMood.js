cf136619c45073a3ccf29333bd00c493be74ccce
junwu <junwu2012@hotmail.com>
2020-05-23 21:41:54 +0100

    change mood to radio

--- happy/src/components/SubmitMood.js
+++ happy/src/components/SubmitMood.js
@@ -1,5 +1,7 @@
 //this is for the user to enter daily mood data with emoji plus submit button 
 import React, {Component} from 'react';
+import logo from '../img/logo192.png';
+import './SubmitMood.css';
 
 class SubmitMood extends Component{
 
@@ -27,15 +29,11 @@ class SubmitMood extends Component{
             mood: mood,
             text: text
         });
-        // ({
-        //     mood: mood,
-        //     text: text
-        // });
 
-        // this.setState({
-        //     mood: 0,
-        //     text: ""
-        // });
+        this.setState({
+            mood: 0,
+            text: ""
+        });
     }
 
     handleMoodChange(evt){
@@ -49,13 +47,50 @@ class SubmitMood extends Component{
     render(){
         return (
             <form className="mood-form" onSubmit={this.handleSubmit}>
-                <label>Please choose your mood</label>
-                <input type="number" value={this.state.mood} onChange={this.handleMoodChange}>
-                </input>
-                <label>Please input your text</label>
+                <label>Please choose your mood</label><br/>
+                <div className="mood-radio">
+                    <div className="mood">
+                        <img src={logo} className="mood" alt="terrible" />
+                        <input type="radio" id="terrible" name="mood" value="1" 
+                        checked={this.state.mood === '1'}
+                        onChange={this.handleMoodChange} />
+                        <label className="mood-desc" htmlFor="terrible">terrible</label>
+                    </div>
+                    <div className="mood">
+                        <img src={logo} className="mood" alt="bad" />
+                        <input type="radio" id="bad" name="mood" value="2" 
+                        checked={this.state.mood === '2'}
+                        onChange={this.handleMoodChange} />
+                        <label className="mood-desc" htmlFor="bad">bad</label>
+                    </div>
+                    <div className="mood">
+                        <img src={logo} className="mood" alt="ok" />
+                        <input type="radio" id="ok" name="mood" value="3" 
+                        checked = {this.state.mood === '3'}
+                        onChange={this.handleMoodChange} />
+                        <label className="mood-desc" htmlFor="ok">ok</label>
+                    </div>
+                    <div className="mood">
+                        <img src={logo} className="mood" alt="good" />
+                        <input type="radio" id="good" name="mood" value="4" 
+                        checked = {this.state.mood === '4'}
+                        onChange={this.handleMoodChange} />
+                        <label className="mood-desc" htmlFor="good">good</label>
+                    </div>
+                    <div className="mood">
+                        <img src={logo} className="mood" alt="awesome" />
+                        <input type="radio" id="awesome" name="mood" value="5" 
+                        checked = {this.state.mood === '5'}
+                        onChange={this.handleMoodChange} />
+                        <label className="mood-desc" htmlFor="awesome">awesome</label>
+                    </div>
+                </div>
+                {/* <input type="number" value={this.state.mood} onChange={this.handleMoodChange}>
+                </input><br/> */}
+                <label>Please input your text</label><br/>
                 <input type="text" placeholder="add some text" 
                 value = {this.state.text} onChange={this.handleTextChange}>
-                </input>
+                </input><br/>
                 <input type="submit" value="save"/>
             </form>
         )
