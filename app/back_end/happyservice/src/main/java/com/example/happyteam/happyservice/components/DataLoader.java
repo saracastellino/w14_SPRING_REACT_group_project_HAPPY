8b395bd2384f736231aaa22246590ba2e0356fa3
saracastellino <sara.castellino0@gmail.com>
2020-05-27 23:36:02 +0100

    adds background item to moodlogs

--- app/back_end/happyservice/src/main/java/com/example/happyteam/happyservice/components/DataLoader.java
+++ app/back_end/happyservice/src/main/java/com/example/happyteam/happyservice/components/DataLoader.java
@@ -29,12 +29,17 @@ public class DataLoader implements ApplicationRunner {
         User sara = new User("Sara", 55, "Paleo Diet", "Opera", 2);
         userRepository.save(sara);
 
-
-        DiaryEntry diaryEntry3 = new DiaryEntry("19 05 2020", 5, "Reading Harry Potter to my son", julia);
-        diaryEntryRepository.save(diaryEntry3);
+        DiaryEntry diaryEntry1 = new DiaryEntry("19 05 2020", 5, "Ate pizza, coded and loved JS", julia);
+        diaryEntryRepository.save(diaryEntry1);
         DiaryEntry diaryEntry2 = new DiaryEntry("20 05 2020", 4, "Using Java", julia);
         diaryEntryRepository.save(diaryEntry2);
-        DiaryEntry diaryEntry1 = new DiaryEntry("22 05 2020", 5, "Ate pizza, coded and loved JS", julia);
+        DiaryEntry diaryEntry3 = new DiaryEntry("21 05 2020", 5, "Reading Harry Potter to my son", julia);
+        diaryEntryRepository.save(diaryEntry3);
+        DiaryEntry diaryEntry4 = new DiaryEntry("23 05 2020", 3, "Had a walk in the park", julia);
+        diaryEntryRepository.save(diaryEntry3);
+        DiaryEntry diaryEntry5 = new DiaryEntry("24 05 2020", 2, "Using JS", julia);
+        diaryEntryRepository.save(diaryEntry2);
+        DiaryEntry diaryEntry6 = new DiaryEntry("25 05 2020", 3, "Chatted with my friends", julia);
         diaryEntryRepository.save(diaryEntry1);
 
     }
